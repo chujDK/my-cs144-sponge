1d548cd0ddca205de771fd3409b5e153834ca20f
chujDK <1768485949@qq.com>
2022-11-29 16:04:26 +0800

    add: webget on cs144 tcp

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh"
 #include "util.hh"
 
 #include <cstdlib>
@@ -14,7 +14,7 @@ void get_URL(const string &host, const string &path) {
     http_request += "\r\n";
 
     Address host_address{host, "http"};
-    TCPSocket tcp_socket;
+    CS144TCPSocket tcp_socket;
     tcp_socket.connect(host_address);
     tcp_socket.write(http_request);
 
@@ -24,6 +24,8 @@ void get_URL(const string &host, const string &path) {
         got += tcp_socket.read();
     }
     std::cout << got;
+
+    tcp_socket.wait_until_closed();
 }
 
 int main(int argc, char *argv[]) {
